db79e931b6073db965451d704d4a4801ecb7c93f
Jaeyoung Lee <jylee0121@gmail.com>
2020-01-10 22:07:20 +0900

    update: use opencv-gui
     - add --env QT_X11_NO_MITSHM=1 for opencv-gui
     - update Dockerfile
    
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- run_docker.sh
+++ run_docker.sh
@@ -3,11 +3,12 @@ XAUTH=/tmp/.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-docker run  --gpus=all -dt -it \
+docker run  --gpus=all --rm -it \
         --volume=$XSOCK:$XSOCK:rw \
         --volume=$XAUTH:$XAUTH:rw \
         --env="XAUTHORITY=${XAUTH}" \
         --env="DISPLAY=${DISPLAY}" \
+        --env QT_X11_NO_MITSHM=1 \
         --env="NVIDIA_DRIVER_CAPABILITIES=all" \
         -v /home/jylee/work2:/work2 \
         -w /work2 \
