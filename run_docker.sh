bf95a68d2becca8a13c7cb4253c52d8c04b0bf21
Jaeyoung Lee <jylee0121@gmail.com>
2020-01-13 20:28:31 +0900

    update: update docker setup
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- run_docker.sh
+++ run_docker.sh
@@ -3,17 +3,23 @@ XAUTH=/tmp/.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-docker run  --gpus '"device=1"' --dt -it \
+#        --device /dev/video0 \ # for webcam
+#        --device /dev/bus/usb \ # for flir camera
+
+docker run  --gpus device=1 -dt -it \
         --volume=$XSOCK:$XSOCK:rw \
         --volume=$XAUTH:$XAUTH:rw \
         --env="XAUTHORITY=${XAUTH}" \
         --env="DISPLAY=${DISPLAY}" \
         --env QT_X11_NO_MITSHM=1 \
         --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+        --device /dev/video0 \
+        --device /dev/bus/usb \
         -v /home/jylee/work2:/work2 \
         -w /work2 \
         --name tf-trt6-con \
-   tf-trt6:latest
+        --net=host \
+   tf-trt6-spinnaker:latest
 
 # options
 #        --privileged \
